6a0b36ffa6f85a0ac724a1579b3b860ee6b1cda6
hippo24 <lodenx24@gmail.com>
2024-12-30 17:47:59 +0900

    Reapply "자바 1일차 = 변수 선언 예제"
    This reverts commit 71b285fc71e8ba2aadd54b8c32e16e00fcde8df2.

---
+++ java_khy/src/day01/Ex05_VariableBoolean.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Ex05_VariableBoolean {
+
+	public static void main(String[] args) {
+		boolean isAdult = true;//또는 false
+		int age = 10;
+		isAdult = age > 18;
+		System.out.println(age + "살은 성인?" + isAdult);
+	}
+
+}
