accf125da578896b27ff075c480be2738da3f5bc
hippo24 <lodenx24@gmail.com>
2024-12-31 17:24:26 +0900

    자바 2일차 : for문 예제

--- java_khy/src/day02/Ex19_ForMultipleTable.java
+++ java_khy/src/day02/Ex19_ForMultipleTable.java
@@ -15,7 +15,7 @@ public class Ex19_ForMultipleTable {
 		
 			int num = 2;
 		for(int i = 1; i<=9; i++) {
-			System.out.println(num + " x " + i +  " = " + num*i);
+			System.out.println(num + " x " + i +  " = " + num*i); 
 		}
 
 	}
