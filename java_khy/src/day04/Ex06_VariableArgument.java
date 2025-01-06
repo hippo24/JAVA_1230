80d1e78bef55ae0843bc021a779808c8c4ff958f
hippo24 <lodenx24@gmail.com>
2025-01-06 09:08:34 +0900

    자바 4일차 : 가변 매개변수 예제

---
+++ java_khy/src/day04/Ex06_VariableArgument.java
@@ -0,0 +1,18 @@
+package day04;
+
+public class Ex06_VariableArgument {
+
+	public static void main(String[] args) {
+		System.out.println(sum(1));
+		System.out.println(sum(1,2));
+		System.out.println(sum(1,2,3));
+	}
+
+	public static int sum(int ... nums) {
+		int sum = 0;
+		for(int num : nums) {
+			sum += num;
+		}
+		return sum;
+	}
+}
