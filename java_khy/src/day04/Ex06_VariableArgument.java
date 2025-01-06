c149dbd4a115ec7714aa11a41f640a6c003d1739
hippo24 <lodenx24@gmail.com>
2025-01-06 09:58:00 +0900

    자바 4일차 : 가변 매개변수 예제

--- java_khy/src/day04/Ex06_VariableArgument.java
+++ java_khy/src/day04/Ex06_VariableArgument.java
@@ -6,8 +6,9 @@ public class Ex06_VariableArgument {
 		System.out.println(sum(1));
 		System.out.println(sum(1,2));
 		System.out.println(sum(1,2,3));
-	}
 
+	}
+	//가변 매개 변수를 이용하여 여러개의 정수를 합하는 메소드 예제
 	public static int sum(int ... nums) {
 		int sum = 0;
 		for(int num : nums) {
