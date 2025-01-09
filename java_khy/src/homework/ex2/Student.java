671d55f5b22e2e9c0e2d0803afa3c9f494ce2893
hippo24 <lodenx24@gmail.com>
2025-01-09 17:31:07 +0900

    자바 08일차 : 주말 과제 문제

---
+++ java_khy/src/homework/ex2/Student.java
@@ -0,0 +1,14 @@
+package homework.ex2;
+
+import lombok.Data;
+
+//getter, setter, toString, equals등을 제공
+@Data 
+public class Student {
+	
+	public int grade, classNum, num;
+	public String name;
+	
+	//학생 개인의 성적을 담고 있는 배열
+	public Subject [] list;
+}
---
+++ java_khy/src/homework/ex2/StudentMain.java
@@ -0,0 +1,37 @@
+package homework.ex2;
+
+public class StudentMain {
+
+	// 학생 성적 관리 프로그램을 작성하세요.
+	public static void main(String[] args) {
+		
+		Student [] stdList;
+		Subject [] subjectList;
+		int stdCount = 0;
+		int subjectCount = 0;
+		Student tmp = new Student();
+		System.out.println(tmp);
+		
+		/* 1. 학생 등록
+		 *  - 학년, 반, 번호, 이름을 입력받아 등록
+		 *  - 이미 등록된 학생 정보(학년, 반, 번호가 같은 경우)라면 등록하지 않음
+		 * 2. 학생 수정
+		 *  - 학년, 반, 번호를 입력받아 학생이 있는지 찾고, 있으면 수정
+		 *  - 수정하는 정보는 학년, 반, 번호, 이름을 수정
+		 *  - 이미 등록된 학생 정보로 수정하려고 하면 수정하지 않음
+		 * 3. 학생 삭제
+		 * 4. 과목 등록
+		 * 5. 과목 수정
+		 * 6. 과목 삭제
+		 * 7. 성적 등록
+		 *  -
+		 * 8. 성적 수정
+		 * 9. 성적 삭제
+		 * 10. 학생 조회
+		 * 11. 과목 조회
+		 * 12. 성적 조회
+		 */
+
+	}
+
+}
