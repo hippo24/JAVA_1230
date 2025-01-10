837fb1c144f891d96352016c7be4c758eea4f3bf
hippo24 <lodenx24@gmail.com>
2025-01-10 17:17:11 +0900

    자바 09일차 : Regex 예제

--- java_khy/src/day09/Ex03_Regex.java
+++ java_khy/src/day09/Ex03_Regex.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Ex03_Regex {
 
 	public static void main(String[] args) {
-
+		
 		//주어진 문자열이 숫자로만 되어 있는지 확인하는 예제
 		String str = "00123123a";
 		String regex = "^\\d+$";
@@ -15,6 +15,7 @@ public class Ex03_Regex {
 		else {
 			System.out.println(str + "에 숫자가 아닌 문자가 있습니다.");
 		}
+
 	}
 
 }
