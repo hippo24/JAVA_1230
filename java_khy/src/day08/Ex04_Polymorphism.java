d0e1b052881aec07369aa610fb2970b7226dc418
hippo24 <lodenx24@gmail.com>
2025-01-10 09:04:52 +0900

    자바 08일차 : 다형성 예제

--- java_khy/src/day08/Ex04_Polymorphism.java
+++ java_khy/src/day08/Ex04_Polymorphism.java
@@ -10,13 +10,13 @@ public class Ex04_Polymorphism {
 		
 		KiaCar kia = new KiaCar();
 		BenzCar benz = new BenzCar();
-
+		
 		repair(benz);
 		repair(kia);
 		
 	}
 	/*
-	 * 아래와 같이 자동차별로 기능을 만들면 자동차 종류별로 메소드 오버로딩을 여러개 해야함
+	//아래와 같이 자동차별로 기능을 만들면 자동차 종류별로 메소드 오버로딩을 여러개 해야 함
 	public static void repair(KiaCar kia) {
 		kia.repair();
 	}
@@ -28,5 +28,4 @@ public class Ex04_Polymorphism {
 	public static void repair(Car car) {
 		car.repair();
 	}
-
 }
