0ae23fa5d3d27c4ee7d09fcef9d6f07171fc7bd7
hippo24 <lodenx24@gmail.com>
2025-01-10 17:16:39 +0900

    자바 09일차 : Try catch 예제

--- java_khy/src/day09/Ex11_TryCatch.java
+++ java_khy/src/day09/Ex11_TryCatch.java
@@ -8,18 +8,19 @@ public class Ex11_TryCatch {
 			//System.out.println(1/0);
 			int [] arr = new int[10];
 			arr[10] = 10;
-			
+			System.out.println("123");
 		}catch(ArithmeticException e) {
 			System.out.println("0으로 나눌 수 없습니다.");
 		}catch(RuntimeException e) {
-			System.out.println("실행 예외가 발생했습니다");
+			System.out.println("실행 예외가 발생했습니다.");
 		}catch(Exception e) {
 			System.out.println("예외가 발생했습니다.");
 		}
 		System.out.println("프로그램을 종료합니다.");
 		System.out.println("---------");
+		print();
 	}
-
+	
 	public static void print() {
 		try {
 			System.out.println(1/0);
