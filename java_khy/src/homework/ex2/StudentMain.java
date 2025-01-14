6cc3497080e6c4476e7e7f7690e855a5dff422b9
hippo24 <lodenx24@gmail.com>
2025-01-14 09:29:02 +0900

    과제2 : 학생 기능 설명 추가

--- java_khy/src/homework/ex2/StudentMain.java
+++ java_khy/src/homework/ex2/StudentMain.java
@@ -1,5 +1,10 @@
 package homework.ex2;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class StudentMain {
 
 	/* 학생 성적 관리 프로그램을 작성하세요.
@@ -37,15 +42,143 @@ public class StudentMain {
 	 * 12. 성적 조회
 	 * 	- 학년, 반, 번호를 입력하면 학생 있으면 학생 성적을 조회
 	 * */
+	//등록된 과목을 관리할 과목 리스트
+	static ArrayList<Subject> subjectList = new ArrayList<Subject>();
+	//등록된 학생을 관리할 학생 리스트
+	static ArrayList<Student> studentList = new ArrayList<Student>();
+	//입력을 위한 Scanner
+	static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
-		Student [] stdList;
-		Subject [] subjectList;
-		int stdCount = 0;
-		int subjectCount = 0;
-		Student tmp = new Student();
-		System.out.println(tmp);
+		int menu = 0; //예외처리 때문에 초기화를 해야 함.
+		
+		do {
+			//메뉴 출력
+			printMenu();
+			
+			try {
+				//메뉴 선택
+				menu = scan.nextInt();
+				//메뉴 실행
+				runMenu(menu);
+			}
+			//입력 타입이 안 맞는 경우
+			catch(InputMismatchException e) {
+				System.out.println("올바른 입력이 아닙니다!");
+				scan.nextLine();//잘못 입력한 값을 입력 버퍼에서 지우기 위함
+			}
+			
+		}while(menu != 13);
 
 	}
 
+	private static void printMenu() {
+
+		System.out.println("1. 학생 등록");
+		System.out.println("2. 학생 수정");
+		System.out.println("3. 학생 삭제");
+		System.out.println("4. 과목 등록");
+		System.out.println("5. 과목 수정");
+		System.out.println("6. 과목 삭제");
+		System.out.println("7. 성적 등록");
+		System.out.println("8. 성적 수정");
+		System.out.println("9. 성적 석제");
+		System.out.println("10.학생 조회");
+		System.out.println("11.과목 조회");
+		System.out.println("12.성적 조회");
+		System.out.println("13.종료");
+		System.out.print("메뉴 선택 : ");
+		
+	}
+	
+	private static void runMenu(int menu) {
+
+		switch(menu) {
+		case 1:
+			insertStudent();
+			break;
+		case 2:
+			updateStudent();
+			break;
+		case 3:
+			deleteStudent();
+			break;
+		case 4:
+			System.out.println("과목 등록 기능 구현 예정");
+			break;
+		case 5:
+			System.out.println("과목 수정 기능 구현 예정");
+			break;
+		case 6:
+			System.out.println("과목 삭제 기능 구현 예정");
+			break;
+		case 7:
+			System.out.println("성적 등록 기능 구현 예정");
+			break;
+		case 8:
+			System.out.println("성적 수정 기능 구현 예정");
+			break;
+		case 9:
+			System.out.println("성적 삭제 기능 구현 예정");
+			break;
+		case 10:
+			System.out.println("학생 조회 기능 구현 예정");
+			break;
+		case 11:
+			System.out.println("과목 조회 기능 구현 예정");
+			break;
+		case 12:
+			System.out.println("과목 사제 기능 구현 예정");
+			break;
+		case 13:
+			System.out.println("프로그램 종료");
+			break;
+		default:
+			System.out.println("올바른 메뉴가 아닙니다.");
+			break;
+		}
+		
+	}
+
+	private static void deleteStudent() {
+		//학년, 반, 번호를 입력
+		
+		//입력받은 정보로 객체 생성
+		
+		//생성한 객체를 이용하여 리스트에서 삭제
+		
+		//삭제에 성공하면 성공알림문구
+		
+		//실패하면 실패 알림문구 출력
+		
+	}
+
+	private static void updateStudent() {
+		//학년, 반, 번호 입력
+		
+		//입력한 학생 정보를 객체 생성
+		
+		//생성한 객체가 리스트에 있으면 번지를 가져옴
+		
+		//번지가 음수이면 안내문구 출력 후 종료
+		
+		//아니면 수정할 학년, 반, 번호, 이름을 입력
+		
+		//입력받은 정보로 객체를 생성
+		
+		//번지에 있는 객체를 위에서 생성한 객체로 변경
+		
+	}
+
+	private static void insertStudent() {
+		//학년, 반, 번호, 이름 입력
+		
+		//입력 받은 학년, 반, 번호, 이름을 이용하여 객체 생성
+		
+		//생성한 객체가 리스트에 있는지 확인하여 있으면 종료
+		
+		//없으면 리스트에 추가 후 안내 문구
+		
+	}
 }
