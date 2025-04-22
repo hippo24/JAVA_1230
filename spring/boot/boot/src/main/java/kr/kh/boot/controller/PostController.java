30f03717a055e57d81a7fe35f8222f2f5b64abff
hippo24 <lodenx24@gmail.com>
2025-04-22 09:24:10 +0900

    Spring Boot

---
+++ spring/boot/boot/src/main/java/kr/kh/boot/controller/PostController.java
@@ -0,0 +1,35 @@
+package kr.kh.boot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import kr.kh.boot.model.vo.BoardVO;
+import kr.kh.boot.model.vo.PostVO;
+import kr.kh.boot.service.PostService;
+
+
+@Controller
+public class PostController {
+	
+	@Autowired
+	PostService postService;
+
+	@GetMapping("/post/list/{bo_num}")
+	public String postList(Model model, @PathVariable int bo_num) {
+		//등록된
+		List<BoardVO> boardList = postService.getBoardList();
+
+		//
+		List<PostVO> list = postService.getPostList(bo_num);
+		model.addAttribute("list", list);
+		model.addAttribute("url", "/post/list");
+		model.addAttribute("boardList", list);
+		return "post/list";
+	}
+	
+}
