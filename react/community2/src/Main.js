42eab2f04ba267d1aa3ddf92031e78e49015ba22
hippo24 <lodenx24@gmail.com>
2025-04-25 14:43:16 +0900

    0425

--- react/community2/src/Main.js
+++
@@ -1,8 +0,0 @@
-
-function Main(){
-	return(<div>
-		<h1>메인 페이지</h1>
-	</div>);
-}
-
-export default Main;
--- react/react2/src/Button.js
+++
@@ -1,9 +0,0 @@
-
-function Button({click, className, text="버튼"}){
-
-	return (
-		<button onClick={click} className={className}	>{text}</button>
-	);
-}
-
-export default Button;
