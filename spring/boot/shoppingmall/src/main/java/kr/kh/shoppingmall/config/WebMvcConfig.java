cdb9c02f3d114f2525612cfe74901a2ebd8cc5eb
hippo24 <lodenx24@gmail.com>
2025-04-30 12:34:27 +0900

    쇼핑몰 : 제품 등록 구현 완료

---
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package kr.kh.shoppingmall.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "kr.kh.shoppingmall")
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Value("${spring.path.upload}")
+	String uploadPath;
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/file/**").addResourceLocations("file:///"+uploadPath);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
--- spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/service/ProductService.java
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/service/ProductService.java
@@ -1,19 +1,26 @@
 package kr.kh.shoppingmall.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.shoppingmall.dao.ProductDAO;
 import kr.kh.shoppingmall.model.vo.CategoryVO;
 import kr.kh.shoppingmall.model.vo.ProductVO;
+import kr.kh.shoppingmall.utils.UploadFileUtils;
 
 @Service
 public class ProductService {
 
 	@Autowired
 	ProductDAO productDAO;
+
+	@Value("${spring.path.upload}")
+	String uploadPath;
 
 	public List<CategoryVO> getCategory() {
 		return productDAO.selectCategoryList();
@@ -62,5 +69,35 @@ public class ProductService {
 
 	public List<ProductVO> getProductList(int ca_num) {
 		return productDAO.selectProductList(ca_num);
+	}
+	public boolean insertProduct(ProductVO product, MultipartFile thumb) {
+		if(product == null || thumb == null || thumb.getOriginalFilename().isEmpty()){
+			return false;
+		}
+		String pr_code = productDAO.selectNextPrCode(product.getPr_ca_num());
+		product.setPr_code(pr_code);
+		boolean res = productDAO.insertProduct(product);
+		if(!res){
+			return false;
+		}
+		//썸네일 작업
+		String fileName = thumb.getOriginalFilename();
+		String surffix = getSuffix(fileName);
+		String newFileName = product.getPr_code() + surffix;
+		String thumbnail;
+		try {
+			thumbnail = UploadFileUtils.uploadFile(uploadPath, newFileName, thumb.getBytes(), "product");
+			product.setPr_thumb(thumbnail);
+			productDAO.updateProduct(product);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	private String getSuffix(String fileName) {
+
+		int index = fileName.lastIndexOf(".");
+		return index < 0 ? null : fileName.substring(index);
 	}
 }
